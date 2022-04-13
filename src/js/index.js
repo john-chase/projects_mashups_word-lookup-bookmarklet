688acedfeceab0ea4526797eeb8f8ed1b1c69ad9
John Chase <john@theartoftechllc.com>
2022-04-13 07:44:23 -0600

    Release alt version

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 (() => {
     /*inject styles*/
     const styleElem = document.head.appendChild(document.createElement("style"));
-    // styleElem.innerText = `
-    // /*GENERAL*/ .p { border: 1px solid dodgerblue; border-radius: .5rem; padding: .5rem; margin: 1rem 5rem; font-size: 15px; line-height: 21px; } .hidden { display: none; } /*DEFINITION GROUP*/ .def-div { background-color: dodgerblue; position: fixed; z-index: 99999999; min-width: 100%; height: 32px; top: 0; text-align: left; } .def-span { padding: 0 0 12px 4px; text-align: center; color: blue; font-weight: bold; font-style: italic; margin: 0.5rem; } .def-help { font-style: italic; margin: 0.5rem; } /*TOOLTIPS*/ .def-tooltip { position: relative; transition: all 0.3s ease; text-decoration: none; cursor: grabbing; z-index: 999999; box-sizing: border-box; } .def-tooltip:after { content: attr(data-tooltip); position: absolute; left: 50%; top: 1rem; transform: translate3d(-50%, 1rem, 0); transition: all 0.3s ease; transition-delay: 1.2s; width: -webkit-max-content; width: -moz-max-content; width: max-content; max-width: 200px !important; opacity: 0; pointer-events: none; border-radius: 4px; background: dodgerblue; color: white; font-size: 11px; font-weight: bold; text-align: left; line-height: 14px; text-transform: none; white-space: pre-line; padding: 4px 8px; } .def-tooltip:hover { background-color: rgba(0, 0, 0, 0.12); } .def-tooltip:hover:after { opacity: 1; } .def-tooltip-not-found:after { color: white; font-weight: bold; background: red; } /*TOGGLE SWITCH*/ input[type=checkbox], .def-input { margin: 0; -webkit-appearance: none; appearance: none; padding: 0 0 0 30px; border-radius: 16px; background: radial-gradient(circle 12px, white 100%, transparent calc(100% + 1px)) #ccc -16px; transition: 0.3s ease-in-out; position: inherit; height: 32px; width: 64px; top: 0; bottom: 0; box-shadow: none; } .def-input::before { content: "OFF"; font: bold 12px/32px Verdana; color: white; text-shadow: 0 1px black; } .def-input:checked { padding: 0 0 0 8px; background-color: rgb(37, 151, 62); background-position: 16px; height: 32px; width: 64px; } .def-input:checked::before { content: "ON"; }
-    // `;
+    styleElem.innerText = `
+    /*GENERAL*/ .p { border: 1px solid dodgerblue; border-radius: .5rem; padding: .5rem; margin: 1rem 5rem; font-size: 15px; line-height: 21px; } .hidden { display: none; } /*DEFINITION GROUP*/ .def-div { background-color: dodgerblue; position: fixed; z-index: 99999999; min-width: 100%; height: 32px; top: 0; text-align: left; } .def-span { padding: 0 0 12px 4px; text-align: center; color: blue; font-weight: bold; font-style: italic; margin: 0.5rem; } .def-help { font-style: italic; margin: 0.5rem; } .def-audio { width: 20px; height: 20px; margin-bottom: -.3rem; padding-left: 4px; } /*TOOLTIPS*/ .def-tooltip { content: (data-tooltip); position: relative; transition: all 0.3s ease; text-decoration: none; cursor: grabbing; z-index: 999999; box-sizing: border-box; } .def-tooltip-not-found { color: white; font-weight: bold; background: red; } /*TOGGLE SWITCH*/ input[type=checkbox], .def-input { margin: 0; -webkit-appearance: none; appearance: none; padding: 0 0 0 30px; border-radius: 16px; background: radial-gradient(circle 12px, white 100%, transparent calc(100% + 1px)) #ccc -16px; transition: 0.3s ease-in-out; position: inherit; height: 32px; width: 64px; top: 0; bottom: 0; box-shadow: none; } .def-input::before { content: "OFF"; font: bold 12px/32px Verdana; color: white; text-shadow: 0 1px black; } .def-input:checked { padding: 0 0 0 8px; background-color: rgb(37, 151, 62); background-position: 16px; height: 32px; width: 64px; } .def-input:checked::before { content: "ON"; }
+    `;
     /*set up elements*/
     const body = document.getElementsByTagName("body")[0];
     const defDiv = document.createElement("div");
@@ -74,10 +74,10 @@ const wordLookupRequest = async (lookup) => {
     let response = await fetch(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${lookup}`
     );
-    const sorry = `Sorry, we could not find "${lookup}" in the dictionary, or the service is down. You can try the search again at later time or head to the <a href="https://www.dictionary.com/">web</a> instead.`
+    const sorry = `Sorry, we could not find "${lookup}" in the dictionary, or the service is down. You can try the search again at later time or head to the <a href="https://www.dictionary.com/" target="_blank">web</a> instead.`
     if (response.status >= 200 && response.status <= 299) {
         let json = await response.json();
-        let phon = "";
+        let phon = ""; 
         if (json[0]) {
             console.log(json[0]);
             const word = json[0].word;
