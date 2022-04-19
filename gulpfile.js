cc5a8982999aca2835dd017d89c581af8d2c49d0
John Chase <john@theartoftechllc.com>
2022-04-19 08:12:10 -0600

    gulp build release

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const {src, dest, series, parallel, watch} = require('gulp');
+const del = require('del');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const cleanCss = require('gulp-clean-css');
+const gap = require('gulp-append-prepend');
+const origin = './src';
+const destination = './dist';
+function html(done) {
+    src(`${origin}/index.html`)
+        .pipe(dest(destination));
+    done();
+}
+function css(done) {
+    src(`${origin}/css/bookmarklet.css`)
+        .pipe(cleanCss())
+        .pipe(dest(`${destination}/css`))
+    done();
+}
+function js(done) {
+    src(`${origin}/js/index.js`)
+        .pipe(babel({
+            presets: ["@babel/preset-env"]
+        }))    
+        .pipe(uglify())
+        .pipe(gap.prependText('javascript:'))        
+        .pipe(dest(`${destination}/js`));
+    done();
+}
+function images(done) {
+    src(`${origin}/**/images/*.*`)
+        .pipe(dest(`${destination}`));
+    done();
+}
+async function clean(done) {
+    await del(destination);
+    done();
+}
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.default = series(clean, parallel(html, js, css, images));
